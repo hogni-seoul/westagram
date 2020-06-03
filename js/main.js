a2a30c34f089b6d9ae00c0687dd7ed15c451e0ed
hogni <hogni@hogniui-MacBook-Pro.local>
2020-06-03 13:30:37 +0900

    add features to main page

--- js/main.js
+++ js/main.js
@@ -1,7 +1,82 @@
+// 댓글 관련
 const footerComments = document.querySelector(".footer__comments");
 const commentInput = document.querySelector("#form-comment input");
 const addCommentButton = document.querySelector(".add-btn");
 
+// 검색 관련
+const searchInput = document.querySelector(".search-input");
+const showingText = document.querySelector(".search-fake span");
+const floatingBox = document.querySelector(".floating-box");
+const resultDefault = document.querySelector(".result-default");
+const results = document.querySelectorAll(".result:not(.result-default)");
+const cancelSearching = document.querySelector(
+  ".search-input-icon.fa-times-circle"
+);
+
+const searching = () => {
+  const searchingChr = searchInput.value.trim();
+
+  if (searchingChr.length > 0) {
+    showingText.textContent = searchingChr;
+    floatingBox.classList.remove("unshown");
+
+    let resultCounts = 0;
+    for (let i = 0; i < results.length; i++) {
+      const resultId = results[i].querySelector(".result-id").textContent;
+      const resultSmall = results[i].querySelector(".result-small").textContent;
+
+      if (
+        resultId.includes(searchingChr) ||
+        resultSmall.includes(searchingChr)
+      ) {
+        results[i].classList.remove("unshown");
+        resultCounts += 1;
+      } else {
+        results[i].classList.add("unshown");
+      }
+    }
+    // found nothing
+    if (!resultCounts) {
+      resultDefault.classList.remove("unshown");
+    } else {
+      resultDefault.classList.add("unshown");
+    }
+  }
+
+  clearSearchingChr();
+};
+
+const clearSearchingChr = () => {
+  const searchingChr = searchInput.value.trim();
+
+  if (searchingChr.length === 0) {
+    showingText.textContent = "검색";
+    floatingBox.classList.add("unshown");
+  }
+};
+
+searchInput.addEventListener("keyup", function () {
+  searching();
+});
+
+searchInput.addEventListener("focusin", function () {
+  searching();
+});
+
+searchInput.addEventListener("focusout", function () {
+  for (let i = 0; i < results.length; i++) {
+    results[i].classList.add("unshown");
+  }
+  resultDefault.classList.add("unshown");
+  floatingBox.classList.add("unshown");
+});
+
+cancelSearching.addEventListener("mousedown", function () {
+  searchInput.value = "";
+  clearSearchingChr();
+});
+
+// 댓글 달기
 const addComment = () => {
   const text = commentInput.value.trim();
   const div = document.createElement("div");
@@ -11,6 +86,17 @@ const addComment = () => {
 
   footerComments.appendChild(div);
   commentInput.value = "";
+  // 삭제 버튼 추가
+  const delButton = document.createElement("button");
+  delButton.className = "comment-del-btn";
+  delButton.type = "button";
+  delButton.textContent = "삭제";
+
+  div.appendChild(delButton);
+
+  delButton.addEventListener("click", function () {
+    div.remove();
+  });
 };
 
 commentInput.addEventListener("keyup", function () {
